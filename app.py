64b856f24563bf2af1086d0c55d942aa3f48c070
Tharsan7010 <cbseavp@gmail.com>
2023-11-05 09:58:17 +0530

    first project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def collect_information():
+    if request.method == "POST":
+        name = request.form["name"]
+        class_name = request.form["class"]
+        section = request.form["section"]
+        location = request.form["location"]
+
+        # Store the collected data in a text file
+        with open("collected_data.txt", "a") as file:
+            file.write(f" {name}, {class_name}, {section} ,   {location}\n")
+
+        return f"Submission Successfull"
+
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run()
