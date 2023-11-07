60bb3cbc4c294555fd5c9d0845909a370c0f3f7c
Tharsan7010 <cbseavp@gmail.com>
2023-11-07 17:17:50 +0530

    first project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect
+import openpyxl
 
 app = Flask(__name__)
 
@@ -9,14 +10,17 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form.get('name')
-    class_name = request.form.get('class')
+    grade = request.form.get('grade')
     section = request.form.get('section')
     location = request.form.get('location')
 
-    with open('data.txt', 'a') as file:
-        file.write(f'Name: {name}, Class: {class_name}, Section:{section}, Location:{location}\n')
+    # Store the data in an Excel file
+    workbook = openpyxl.load_workbook('data.xlsx')
+    worksheet = workbook.active
+    worksheet.append([name, grade, section, location ])
+    workbook.save('data.xlsx')
 
-    return 'Data has been submitted.'
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
