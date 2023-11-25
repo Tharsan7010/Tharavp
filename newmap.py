768eca43a62d94a81401f13e8cd6d5851865a263
Tharsan7010 <cbseavp@gmail.com>
2023-11-25 23:02:20 +0530

    Initial Commit

--- newmap.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-map_url = "https://www.google.com/maps/place/Goa,+India/@15.3041937,73.1792282,12z/data=!3m1!4b1!4m13!1m7!3m6!1s0x3bc2e0a81e01c03:0x68424a5047976946!2sGoa,+India!3b1!8m2!3d15.152485!4d74.1251848!3m5!1s0x3bc2e0a81e01c03:0x68424a5047976946!2sIndia!5m2!1s0x3bc2e0a81e01c03:0x68424a5047976946!2sIndia"
-
-webbrowser.open(map_url)
